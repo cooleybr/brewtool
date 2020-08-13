58a8643a6d79e2ce1fbe475b409d99d095bf4d35
Brian Cooley <cooleybr@yahoo.com>
2020-08-13 14:53:08 -0400

    added

--- src/app/models/grain.model.ts
+++ src/app/models/grain.model.ts
@@ -1,5 +1,10 @@
 export class Grain {
     name: string;
+    maltster: string;
+    region: string;
     description: string;
+    type: string;
+    lovibond: string;
+    diastatic:string;
     quantity: number;
 }
--- src/app/models/yeast.model.ts
+++ src/app/models/yeast.model.ts
@@ -1,5 +1,9 @@
 export class Yeast {
     name: string;
+    manufacturer: string;
+    flocculation:string;
+    max:string;
+    min:string;
     description: string;
     quantity: string;
 }
